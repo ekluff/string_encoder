7722edee1d88ce73e01219137ae8f5b0132ce0be
Evan Clough <evan@evanclough.com>
2018-12-28 23:58:32 +0000

    Improve regex, remove conditionals.

--- run_length_encoding.rb
+++ run_length_encoding.rb
@@ -1,16 +1,16 @@
 class RunLengthEncoding
   class << self
     def encode(input)
-      input.scan(/(.)(\1*)/).map do |first,rest|
-        length = rest.length > 0 ? rest.length + 1 : nil
+      input.scan(/(.)(\1+)?/).map do |first,rest|
+        length = rest.length + 1 rescue nil
         [ length, first ]
       end.join
     end
 
     def decode(input)
-      input.scan(/(\d*)([\w\s]{1})/).map do |group|
-        length = group[0].to_i
-        length > 0 ? group[1] * length : group[1]
+      input.scan(/(\d+)?([\w\s]{1})/).map do |group|
+        length = ( group[0] || 1 ).to_i
+        group[1] * length
       end.join
     end
   end
